5aefb4d4c657e4ba4780a51340f569509f8f41cb
bagoesrex <bagoesrex@gmail.com>
2026-01-24 20:52:23 +0700

    chore: setup prettier and format project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <div className="w-full min-h-screen flex justify-center items-center flex-col">
-      <h1 className="font-bold text-2xl font-sans">Next Drizzle Postgres</h1>
+    <div className="flex min-h-screen w-full flex-col items-center justify-center">
+      <h1 className="font-sans text-2xl font-bold">Next Drizzle Postgres</h1>
       <p>by Bagoesrex :D</p>
     </div>
   );
