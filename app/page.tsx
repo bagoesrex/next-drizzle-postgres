ff447a92bcaf828e0b76c7b04354dc47a80d5929
bagoesrex <bagoesrex@gmail.com>
2026-01-24 20:46:32 +0700

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center flex-col">
+      <h1 className="font-bold text-2xl font-sans">Next Drizzle Postgres</h1>
+      <p>by Bagoesrex :D</p>
+    </div>
+  );
+}
