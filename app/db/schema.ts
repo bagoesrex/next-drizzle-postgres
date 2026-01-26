964f37109a6e2b220a0be6b9631d27f8350f0260
bagoesrex <bagoesrex@gmail.com>
2026-01-26 21:29:13 +0700

    chore: add drizzle config and postgres migrations

---
+++ app/db/schema.ts
@@ -0,0 +1,9 @@
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  number: text("number").notNull(),
+  email: text("email").notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow(),
+});
