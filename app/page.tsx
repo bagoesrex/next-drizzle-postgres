0fbaa15224f75bad90869a0f26a59f7a4494f879
bagoesrex <bagoesrex@gmail.com>
2026-01-28 06:00:50 +0700

    feat: add max width wrapper component and improve home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,32 @@
 "use client";
 
 import { usePosts } from "@/hooks/use-posts";
+import MaxWidthWrapper from "@/components/layout/max-width-wrapper";
 
 export default function HomePage() {
   const { data: posts, isLoading, isError, error } = usePosts();
 
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4">
-      <h1 className="text-2xl font-bold underline underline-offset-4">Posts</h1>
+    <MaxWidthWrapper className="min-h-screen space-y-8 py-30">
+      <h2 className="font-sans text-3xl font-bold">Posts</h2>
+      <div>
+        {isLoading && <p className="text-gray-500">Loading...</p>}
 
-      {isLoading && <p className="text-gray-500">Loading...</p>}
+        {isError && <p className="text-red-500">{(error as Error).message}</p>}
 
-      {isError && <p className="text-red-500">{(error as Error).message}</p>}
+        {!isLoading && posts?.length === 0 && <p className="text-gray-500">Belum ada data post</p>}
 
-      {!isLoading && posts?.length === 0 && <p className="text-gray-500">Belum ada data post</p>}
-
-      {posts && posts.length > 0 && (
-        <div>
-          {posts.map((post) => (
-            <div key={post.id} className="py-2">
-              <h2 className="font-bold">{post.title}</h2>
-              <p className="text-sm">{post.content}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+        {posts && posts.length > 0 && (
+          <div>
+            {posts.map((post) => (
+              <div key={post.id} className="py-2">
+                <h2 className="font-bold">{post.title}</h2>
+                <p className="text-sm">{post.content}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </MaxWidthWrapper>
   );
 }
