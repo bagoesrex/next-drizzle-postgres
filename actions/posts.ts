951a4fb4cbee19e42bb5e3bb0dd30addaa6cc4ed
bagoesrex <bagoesrex@gmail.com>
2026-01-31 21:32:23 +0700

    feat: implement create post with slug generation

--- actions/posts.ts
+++ actions/posts.ts
@@ -3,9 +3,10 @@
 import { db } from "@/db/drizzle";
 import { posts as postsTable } from "@/db/schema";
 import { ActionResponse } from "@/types/response";
-import { InferSelectModel } from "drizzle-orm";
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 
 export type Post = InferSelectModel<typeof postsTable>;
+export type newPost = InferInsertModel<typeof postsTable>;
 
 export async function getPosts(): Promise<ActionResponse<Post[]>> {
   try {
@@ -16,6 +17,19 @@ export async function getPosts(): Promise<ActionResponse<Post[]>> {
       success: false,
       data: null,
       message: "Failed to fetch posts",
+    };
+  }
+}
+
+export async function createPost(data: newPost): Promise<ActionResponse<null>> {
+  try {
+    await db.insert(postsTable).values(data);
+    return { success: true, data: null };
+  } catch {
+    return {
+      success: false,
+      data: null,
+      message: "Failed to create post",
     };
   }
 }
--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,4 +1,6 @@
-import { getPosts } from "@/actions/posts";
+import { createPost, getPosts } from "@/actions/posts";
+import { NextResponse } from "next/server";
+import slugify from "slugify";
 
 export async function GET() {
   const res = await getPosts();
@@ -8,4 +10,25 @@ export async function GET() {
   }
 
   return Response.json(res.data);
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const { title, content, tags } = body;
+
+  const slug = slugify(title, { lower: true, strict: true });
+
+  const res = await createPost({
+    title,
+    content,
+    tags,
+    slug,
+  });
+
+  if (!res.success) {
+    return NextResponse.json({ message: res.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ success: true });
 }
