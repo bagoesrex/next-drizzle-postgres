ebd84b2605c2443e18cc483d81234ec4d4b6fe1d
bagoesrex <bagoesrex@gmail.com>
2026-01-31 21:01:26 +0700

    feat: render tags list in post card

--- components/posts/post-card.tsx
+++ components/posts/post-card.tsx
@@ -10,7 +10,7 @@ interface PostCardProps {
 dayjs.extend(relativeTime);
 
 export default function PostCard({ post }: PostCardProps) {
-  const { title, content, createdAt } = post;
+  const { title, content, createdAt, tags } = post;
 
   const [now, setNow] = useState(() => Date.now());
   const formattedDate = dayjs(createdAt).from(now);
@@ -21,10 +21,20 @@ export default function PostCard({ post }: PostCardProps) {
   }, []);
 
   return (
-    <div>
+    <div className="rounded-md bg-white/60 p-5">
       <p className="text-muted-foreground font-mono text-sm">{formattedDate}</p>
-      <h3 className="font-sans font-bold uppercase">{title}</h3>
+      <h3 className="mt-2 font-sans font-bold uppercase">{title}</h3>
       <p className="line-clamp-3 text-black/75">{content}</p>
+      <ul className="mt-4.5 flex gap-2">
+        {tags.map((tag, i) => (
+          <li
+            key={i}
+            className="rounded-full bg-green-300/80 px-2.5 py-0.5 text-sm font-semibold text-green-700"
+          >
+            {tag}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
